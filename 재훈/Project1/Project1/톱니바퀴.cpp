f9dd108bc69619e61f1dfe7f876f0d1fb644edbe
JeongGeun <40541480+JeongGeun@users.noreply.github.com>
2018-10-06 22:37:40 +0900

    토마토문제(bfs)

--- 재훈/Project1/Project1/톱니바퀴.cpp
+++ 재훈/Project1/Project1/톱니바퀴.cpp
@@ -1,32 +1,35 @@
 #include<iostream>
 #include<cmath>
+#include<string.h>
 using namespace std;
 
 
 
-int arr[4][9],K,num,dir;
+int arr[4][10],K,num,dir;
 bool flag[4];
 int ret;
+int m;
 
 void circle(int num,int dir)
 {
 	if (dir == 1)
 	{
-		for (int k = 8; k >= 1; k--)
+		for (int t = 8; t >= 1; t--)
 		{
-			arr[num][k + 1] = arr[num][k];
+			arr[num][t + 1] = arr[num][t];
 		}
 		arr[num][1] = arr[num][9];
 		arr[num][9] = 0;
 	}
 	else
 	{
-		for (int k = 0; k <= 7; k++)
+		for (int t = 0; t <= 7; t++)
 		{
-			arr[num][k] = arr[num][k + 1];
+			arr[num][t] = arr[num][t + 1];
 		}
 		arr[num][8] = arr[num][0];
 		arr[num][0] = 0;
+	
 	}
 }
 
@@ -35,12 +38,16 @@ int main()
 {
 	for (int i = 0; i < 4; i++)
 	{
-		for (int j = 1; j < 9; j++)
+		cin >> m;
+		for (int j = 8; j>=1 ; j--)
 		{
-			cin >> arr[i][j];
+			arr[i][j] = m % 10;
+			m /= 10;
 		}
 	}
+
 	cin >> K;
+
 	for (int i = 0; i < K; i++)
 	{
 		cin >> num >> dir;
@@ -49,26 +56,43 @@ int main()
 		int pnum = num + 1;
 		for (int j = 0; j < 4; j++)
 		{
-			if (arr[j][2] != arr[j + 1][7])
+			if (arr[j][3] != arr[j + 1][7])
 				flag[j] = true;
 		}
 		circle(num, dir);
+		int mdir = dir;
 		while (mnum >= 0)
 		{
 			if (flag[mnum])
 			{
-				dir = dir * (-1);
-				circle(mnum, dir);
+				mdir = mdir * (-1);
+				circle(mnum, mdir);
+				mnum--;
 			}
+			else
+				break;
 		}
-
+		int pdir = dir;
+		while (pnum < 4)
+		{
+			if (flag[pnum-1])
+			{
+				pdir = pdir * (-1);
+				circle(pnum, pdir);
+				pnum++;
+			}
+			else
+				break;
+		}
+		memset(flag, false, sizeof(flag));
 	}
+	
 	for (int i = 0; i < 4; i++)
 	{
-		if (arr[i][0])
+		if (arr[i][1])
 			ret += pow(2, i);
 	}
-
+	cout << ret << endl;
 
 
 }
